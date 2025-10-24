def5c3aa79be0b27ec91c47bf78aa329fc0b8601
arifwicakson0 <wicaksonoarif487@gmail.com>
2025-10-24 15:50:40 +0700

    menambahkan layout dasar,mengatur warna latar,komponen image,penggunaan modifikasi agar lebih mudah

--- app/src/main/java/com/example/praktikumpertem4/UIPertama.kt
+++ app/src/main/java/com/example/praktikumpertem4/UIPertama.kt
@@ -1,40 +1,66 @@
 package com.example.praktikumpertem4
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-    fun AktivitasPertama(modifier: Modifier){
-        Column(modifier = Modifier
+    fun AktivitasPertama(modifier: Modifier) {
+    Column(
+        modifier = Modifier
             .padding(top = 100.dp)
             .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.prodi),
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(id = R.string.univ),
+            fontSize = 18.sp
+        )
+        Spacer(modifier = Modifier.height(25.dp))
+        Card(
+            modifier = Modifier
+                .height(height = 160.dp)
+                .fillMaxWidth(fraction = 1f)
+                .padding(all = 12.dp),
+            colors = CardDefaults.cardColors(
+                containerColor = Color.DarkGray
+            )
         ) {
-            Text(
-                text = stringResource(id = R.string.prodi),
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = stringResource(id = R.string.univ),
-                fontSize = 18.sp
-            )
-            Spacer(modifier = Modifier.height(25.dp))
-            Card(
-                modifier = Modifier
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(all = 10.dp)
             ) {
-                // Isi Card nanti di sini
+                val gambar = painterResource(id = R.drawable.logo_uny)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                )
             }
         }
+    }
