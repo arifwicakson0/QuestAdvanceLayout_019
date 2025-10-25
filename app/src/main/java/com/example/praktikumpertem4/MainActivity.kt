b8b44ecd514144f287e336389a31ac95a8c1256c
arifwicakson0 <wicaksonoarif487@gmail.com>
2025-10-25 21:12:33 +0700

    biar bisa di run UIKedua nya

--- app/src/main/java/com/example/praktikumpertem4/MainActivity.kt
+++ app/src/main/java/com/example/praktikumpertem4/MainActivity.kt
@@ -19,7 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PraktikumPertem4Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
                     Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
